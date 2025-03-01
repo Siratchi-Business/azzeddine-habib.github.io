3927fc09487f99465bf319d14fbf29a6a2c3909e
Siratchi-Business <shado1siratchi9business8@gmail.com>
2025-03-01 18:39:20 +0100

    Add SEO and finalize details to enable Google AdSense

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,4 +1,4 @@
-function toggleDarkMode() {
+export function toggleDarkMode() {
     if (document.body.classList.contains('dark-mode')) {
         document.body.classList.remove('dark-mode');
         localStorage.setItem('theme', 'light');
--- javascript/main.js
+++ javascript/main.js
@@ -1,41 +1,45 @@
-import('./animations/intro-animation.js')
-    .then((module) => {
-        console.log("Module loaded:", module);
-    })
-    .catch((error) => {
-        console.error("Error loading the module:", error);
-    });
+const modulesToLoad = [
+    {
+        path: './darkmode.js',
+        onLoad: (module) => {
+            console.log("Darkmode module loaded:", module);
+            document.querySelector('.dark-mode-toggle')
+                .addEventListener('click', module.toggleDarkMode);
+        }
+    },
+    {
+        path: './animations/intro-animation.js',
+        onLoad: (module) => {
+            console.log("Intro animation module loaded:", module);
+        }
+    },
+    {
+        path: './projects.js',
+        onLoad: (module) => {
+            console.log("Projects module loaded:", module);
+        }
+    },
+    {
+        path: './types.js',
+        onLoad: (module) => {
+            console.log("Types module loaded:", module);
+        }
+    },
+    {
+        path: './backend-api-call.js',
+        onLoad: (module) => {
+            console.log("Backend API module loaded:", module);
+        }
+    }
+];
 
-import('./projects.js')
-    .then((module) => {
-        console.log("Module loaded:", module);
-    })
-    .catch((error) => {
-        console.error("Error loading the module:", error);
-    });
-
-import('./types.js')
-    .then((module) => {
-        console.log("Module loaded:", module);
-    })
-    .catch((error) => {
-        console.error("Error loading the module:", error);
-    });
-
-import('./backend-api-call.js')
-    .then((module) => {
-        console.log("Module loaded:", module);
-    })
-    .catch((error) => {
-        console.error("Error loading the module:", error);
-    });
-
-import('./darkmode.js')
-    .then((module) => {
-        console.log("Module loaded:", module);
-    })
-    .catch((error) => {
-        console.error("Error loading the module:", error);
-    });
-
-
+// Importación dinámica y ejecución del callback específico
+modulesToLoad.forEach(({ path, onLoad }) => {
+    import(path)
+        .then((module) => {
+            onLoad(module);
+        })
+        .catch((error) => {
+            console.error(`Error loading the module: ${path}`, error);
+        });
+});
